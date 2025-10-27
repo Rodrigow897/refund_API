9328c51a809af981c198c77db3bb4ebaf38df521
Elias Neto <netoeac.dev@gmail.com>
2025-10-27 11:21:22 -0300

    feat: add CORS support and update database configuration

--- src/dataBase/db.js
+++ src/dataBase/db.js
@@ -1,15 +1,15 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: '',
-  host: 'localhost',
-  database: 'refund',
-  password: '',
-  port: 5432,
+    user: 'elias',
+    host: 'localhost',
+    database: 'refund_db',
+    password: '123321',
+    port: 5432,
 });
 
 pool.connect()
-.then(() => console.log('Connected to the database'))
-.catch((err) => console.error('Database connection error', err));
+    .then(() => console.log('Connected to the database'))
+    .catch((err) => console.error('Database connection error', err));
 
 module.exports = pool;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
-
+app.use(cors());
 const requestRouter = require('../src/routes/requestRouter');
 
 app.get('/', (req, res) => {
